da209e70856649d4d396b7309954bfd1d63d1ebc
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-08-01 21:57:07 +0100

    Implement remaining methods for part 2.

--- my_answers.py
+++ my_answers.py
@@ -49,10 +49,19 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    c = 0
+    while c + window_size < len(text):
+    	inputs.append(text[c : c + window_size])
+    	outputs.append(text[c + window_size])
+    	c += step_size
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+	model = Sequential()
+	model.add(LSTM(200, input_shape=(window_size, num_chars)))
+	model.add(Dense(num_chars, activation='softmax'))
+	return model
